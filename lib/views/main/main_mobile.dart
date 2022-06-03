22cde71cb1a97f2f741fa299b47787b3b3aed2be
francismario <francismariogomez@gmail.com>
2022-06-03 14:48:31 +0000

    fixed a type in main_mobile

--- lib/views/main/main_mobile.dart
+++ lib/views/main/main_mobile.dart
@@ -12,7 +12,7 @@ class _MainMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('MainTablet'),
+        child: Text('MainMobile'),
       ),
     );
   }
